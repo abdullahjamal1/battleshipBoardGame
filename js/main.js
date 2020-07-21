994418a5cfe93291b6896f9f234248a418338c57
aj941ga <45927383+aj941ga@users.noreply.github.com>
2020-07-21 17:28:14 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -186,9 +186,9 @@
             indent = 500;
         }
         
-        fill(255,255,255);
+        
         textSize(20);
-
+	fill(255,255,255);
         text("turn : "+ this.turn , 90+ indent,10 ,100 ,20);
 
         
