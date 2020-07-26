a3f63f59b049de715d92741ff63d884b1ffa004e
aj941ga <45927383+aj941ga@users.noreply.github.com>
2020-07-26 11:38:07 +0300

    Add files via upload

---
+++ js/states/winStateCall.js
@@ -0,0 +1,54 @@
+var winStateCall = function () {
+    // background(0, 0, 0);
+    fill(240, 218, 240);
+    // display victory message too
+    if (player1.win === true) {
+
+        textSize(40);
+        text("Player 1 Wins --- player 1 map !!! turns:- " + player1.turn, 400, 400, 400, 400);
+
+        if (singlePlayerWin === true) {
+
+            bot.drawGridActual();
+        }
+        else {
+
+            player2.drawGridActual();
+        }
+    }
+    else if (player2.win === true) {
+
+        textSize(40);
+        text("Player 2 Wins --- player 2 map !!! turns :- " + player2.turn, 400, 400, 400, 400);
+        player1.drawGridActual();
+    }
+    else {
+        textSize(40);
+        text("BOT Wins --- bot win !!! turns:-" + bot.turn, 400, 400, 400, 400);
+        player1.drawGridActual();
+    }
+
+    var backButton = new button("Menu", 150, 450);
+    backButton.draw();
+
+    if (backButton.insideButton()) {
+        //check to see if the mouse is pressed
+        if (!mouseIsPressed) {
+
+            fill(240, 218, 240, 100);
+            rect(backButton.x, backButton.y, backButton.width, backButton.height);
+        }
+        if (mouseIsPressed) {
+            //if mouse is pressed go to menu
+            winState = false;
+            menu = true;
+
+            singlePlayerWin = false;
+            player1.initializeGrid();
+            player2.initializeGrid();
+            bot.initializeGrid();
+	    createNewMultiplayerObject();
+            createNewSinglePlayerObject();
+        }
+    }
+};
