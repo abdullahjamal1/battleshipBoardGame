a9e29488842cb2ca0ba9bdd67fabc1b25ccd1131
aj941ga <45927383+aj941ga@users.noreply.github.com>
2020-08-14 14:03:33 +0300

    Update draw.js
    main game file , contains the draw function

--- js/draw.js
+++ js/draw.js
@@ -6,6 +6,7 @@ var player1AutoButton = new button("auto", 50, 400);
 var player1ConfirmButton = new button("confirm", 250, 400);
 var player2ConfirmButton = new button("confirm", 750, 400);
 var player2AutoButton = new button("auto", 550, 400);
+
 var posX = 400, posY = 70;
 var singlePlayerButton = new button("singleplayer", posX + 100, posY + 40);
 var multiplayerButton = new button("Multiplayer", posX + 100, posY + 90);
@@ -16,7 +17,11 @@ var statisticsButton = new button("statistics", posX + 100, posY + 290);
 
 var anim = new animation();
 
+/**
+draw
+description: draw function is executed 60 frames per second 
 
+*/
 
 var draw = function () {
 
