a23bf0c6ddc3fe004a2cadbf07f73296a87c3c45
Abdullah_J <45927383+aj941ga@users.noreply.github.com>
2020-09-07 17:55:14 +0300

    Update singlePlayerState.js

--- js/states/singlePlayerState.js
+++ js/states/singlePlayerState.js
@@ -5,7 +5,7 @@ var singlePlayerState = function () {
     // background(0, 255, 255,100);
     var backButton1 = new button("back", 400, 450);
     var DensityLensButtonOn = new button("DensityON", 600, 450);
-    var DensityLensButtonOff = new button("DensityOFF", 760, 450);	
+    var DensityLensButtonOff = new button("DensityOFF", 800, 450);	
 	
     backButton1.draw();
 	
