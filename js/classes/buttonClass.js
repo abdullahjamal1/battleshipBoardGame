c7b9a01b2cf11bb52c98388c380c5c179959218c
Abdullah_J <45927383+aj941ga@users.noreply.github.com>
2020-10-04 10:03:56 +0300

    Update buttonClass.js

--- js/classes/buttonClass.js
+++ js/classes/buttonClass.js
@@ -1,16 +1,17 @@
 // class for buttons - object oriented programming
-var button = function (str, x, y) {
+var button = function (str, x, y, w = 170, h = 40) {
     // buttons constructor
     this.x = x;
     this.y = y;
     this.txt = str;
-    this.height = 40;
-    this.width = 170;
+    this.height = h;
+    this.width = w;
 };
 // buttons method
 button.prototype.draw = function () {
 
-    fill(219, 9, 219, 200);
+   // fill(219, 9, 219, 200);  -> light pink
+    fill(0, 210, 0, 200);
     rect(this.x, this.y, this.width, this.height, 10);
     fill(0, 0, 0);
     textSize(30);
